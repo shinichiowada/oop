e30014e0d8307d605056d70e5e6e6809b45a1670
shinichi <0503baku@gmail.com>
2020-08-28 14:02:51 +0900

    Carクラス作成の課題を提出

--- index.php
+++ index.php
@@ -1,14 +1,55 @@
 <?php
 
-// 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
+class Car
+{
+    private $carName;   // 車名
+    private $carNumber; // 車体番号
+    private $carColor;  // カラー
 
+    // コンストラクタ
+    public function __construct($carName, $carNumber, $carColor)
+    {
+        $this->setName($carName);
+        $this->setNumber($carNumber);
+        $this->setColor($carColor);
+    }
 
+    public function getName()
+    {
+        return $this->carName;
+    }
 
+    public function getNumber()
+    {
+        return $this->carNumber;
+    }
 
+    public function getColor()
+    {
+        return $this->carColor;
+    }
 
-// 以下を実行した時にエラーがでないようにして下さい。
-// Carクラスのインスタンスを生成
-// 引数 : 車名、車体番号、カラー
+    public function setName($carName)
+    {
+        $this->carName = $carName;
+    }
+
+    public function setNumber($carNumber)
+    {
+        $this->carNumber = $carNumber;
+    }
+
+    public function setColor($carColor)
+    {
+        $this->carColor = $carColor;
+    }
+
+    public function Infomation()
+    {
+        echo '車の車種:' . $this->getName() . '、車体番号:' . $this->getNumber() . '、カラー:' . $this->getColor() . 'です。';
+    }
+}
+
 $car1 = new Car('ランクル', 100, 'black');
 
 // 車名(車種)を取得
