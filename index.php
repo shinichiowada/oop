2542c06491cbb0e24635e52aeb23901715df7fbc
shinichi <0503baku@gmail.com>
2020-08-29 22:07:34 +0900

    オブジェクト指向 課題2

--- index.php
+++ index.php
@@ -42,46 +42,68 @@ class Car
     {
         $this->carColor = $carColor;
     }
-    
+
     public function infomation()
     {
         echo '車の車種:' . $this->getName() . '、車体番号:' . $this->getNumber() . '、カラー:' . $this->getColor() . 'です。';
     }
 }
 
+// Carクラスのインスタンスを生成
+// 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスの生成
+class Taxi extends Car
+{
+    private $passenger;
+
+    public function getPassenger()
+    {
+        return $this->passenger;
+    }
+
+    public function pickUp($upNumber)
+    {
+        return $this->passenger += $upNumber;
+    }
+
+    public function infomation()
+    {
+        return '車の車種:' . $this->getName() . '、 ' . '車体番号:' . $this->getNumber() . '、 ' . 'カラー:' . $this->getColor() . '、乗車人数は' . $this->getPassenger() .
+            '人です。';
+    }
+
+    public function lower($out_number)
+    {
+        if (($this->passenger - $out_number) >= 0) 
+        {
+            echo $out_number . '人降車しました。';
+            $this->passenger -= $out_number;
+        } else {
+            echo '降車人数に誤りがあります';
+        }
+    }
+}
+
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 2人降車
+$taxi1->lower(2);
